b1e1757c793bfc732474280c0d9c1ac9f69f852e
Adam Tak <adam.tak2112@gmail.com>
2023-09-02 14:41:19 -0600

    updates

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import '../styles/styles.css';
+
+export default function HomePage() {
+
+    return (
+        <div id="app">
+            <div class="container">
+                <div id="salutation">
+                    <h1 id="hello">Hi, I'm Adam.</h1>
+                    <h2>I'm a Software Engineer who specializes in Quality Assurance and Automated Testing.</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
