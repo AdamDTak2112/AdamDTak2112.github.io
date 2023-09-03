bfeadb437e8fa508fbf8083e96a30347867b5906
Adam Tak <adam.tak2112@gmail.com>
2023-09-03 17:17:37 -0600

    playing around with grid

--- src/app/header.js
+++ src/app/header.js
@@ -1,9 +1,16 @@
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-boostrap/Col';
+import Container from 'react-boostrap/Container';
 
 export default function Header(){
     return (
         <div className='container-fluid'>
-            
+        <Container fluid>
+            <Row>
+                <Col></Col>
+                <Col xs lg='6'></Col>
+            </Row>
+        </Container>
         </div>
     )
 }
