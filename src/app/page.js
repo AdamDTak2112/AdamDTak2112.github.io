ae397e6ecd7e97d2e90a491f723813116985b2d1
Adam Tak <adam.tak2112@gmail.com>
2023-09-03 11:33:39 -0600

    added a picture

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import './page.module.css';
+import Image from 'next/image';
 
 export default function HomePage() {
+    const imageSource = '/1690315709648.jpeg';
 
     return (
         <div id="app">
             <div className="container">
                 <div id="salutation">
                     <h1 id="hello">Hi, I'm Adam.</h1>
+                    <Image 
+                        src={imageSource}
+                        width={500}
+                        height={500}
+                        alt="The man himself"
+                        />
                     <h2>I'm a Software Engineer who specializes in Quality Assurance and Automated Testing.</h2>
                 </div>
             </div>
