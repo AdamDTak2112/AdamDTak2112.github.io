884621d8a8ef40204ba2d2c757317fa1ce7f3225
Adam Tak <adam.tak2112@gmail.com>
2023-09-03 12:58:49 -0600

    Added montserrat font to nextjs redo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import './page.module.css';
 import Image from 'next/image';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+    weight: '300',
+    subsets: ['latin'],
+    display: 'swap',
+});
 
 export default function HomePage() {
     const imageSource = '/1690315709648.jpeg';
 
     return (
-        <div id="app">
+        <div className={montserrat.className} id="app">
             <div className="container">
                 <div id="salutation">
                     <h1 id="hello">Hi, I'm Adam.</h1>
