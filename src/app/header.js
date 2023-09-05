b39932a238026b30dadfc2abfd2ae1124a41b081
Adam Tak <adam.tak2112@gmail.com>
2023-09-04 18:28:54 -0600

    integrate navbar into header

--- src/app/header.js
+++ src/app/header.js
@@ -1,11 +1,22 @@
+'use client'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Link from 'next/link';
+
 
 export default function Header(){
     return (
         <Navbar expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">Tak Software</Navbar.Brand>
+            <Container fluid>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href='/about'>About</Nav.Link>
+                    </Nav.Item>
+                </Nav>
             </Container>
         </Navbar>
     )
