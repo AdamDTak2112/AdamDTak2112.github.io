cc092d12b2bb5e9fd7c614a03de17f342b56c6b5
Adam Tak <adam.tak2112@gmail.com>
2023-09-04 18:30:37 -0600

    copy change

--- src/app/header.js
+++ src/app/header.js
@@ -14,7 +14,7 @@ export default function Header(){
                         <Nav.Link href='/'>Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href='/about'>About</Nav.Link>
+                        <Nav.Link href='/about'>About Me</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Container>
