918721082fcf688304ce639d2b4838e650808d15
Adam Tak <adam.tak2112@gmail.com>
2023-09-04 18:34:36 -0600

    remove unused import

--- src/app/header.js
+++ src/app/header.js
@@ -2,7 +2,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Link from 'next/link';
 
 
 export default function Header(){
