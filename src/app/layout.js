578fc7aed786ecf5786c438e329b8e339648604b
Adam Tak <adam.tak2112@gmail.com>
2023-09-06 13:26:20 -0600

    add footer skeleton

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
 import Header from './header';
+import Footer from '../components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   )
---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import Container from 'react-bootstrap/Container';
+
+export default function Footer(){
+    return (
+        <h1>footer time</h1>
+    )
+}
