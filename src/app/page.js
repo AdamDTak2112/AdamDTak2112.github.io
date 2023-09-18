18e4b3d10229521aa7a822a33de9c1a8a6066b71
Adam Tak <adam.tak2112@gmail.com>
2023-09-18 17:39:09 -0600

    more tailwind fiddles

--- src/app/page.js
+++ src/app/page.js
@@ -13,19 +13,21 @@ export default function HomePage() {
 
     return (
         
-            <div className="container mx-auto">
-                <div id={styles.salutation}> 
-                    <h1 className="text-3xl font-bold mb-25">Hi, I'm Adam.</h1>
-                    <Image 
-                        className='mx-auto my-25'
-                        src={imageSource}
-                        width={250}
-                        height={250}
-                        alt="Picture of Adam Tak, Software Developer"
-                        />
-                    <h2 className="text-2xl">I'm a Software Engineer who specializes in Quality Assurance and Automated Testing of Web Applications.</h2>
+            
+                <div className="container mx-auto flex flex-col"id={styles.salutation}> 
+                    <h1 className="text-3xl font-bold">Hi, I'm Adam.</h1>
+                    <div className="py-25">
+                        <Image 
+                            className='mx-auto my-25'
+                            src={imageSource}
+                            width={250}
+                            height={250}
+                            alt="Picture of Adam Tak, Software Developer"
+                            />
+                    </div>
+                    <h2 className="text-2xl mt-15">I'm a Software Engineer who specializes in Quality Assurance and Automated Testing of Web Applications.</h2>
                 </div>
-            </div>
+            
         
     );
 }
