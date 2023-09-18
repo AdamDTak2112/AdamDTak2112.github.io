4b16e29525a2c7e4bb0cc35c51d4d3c442de4671
Adam Tak <adam.tak2112@gmail.com>
2023-09-18 13:59:57 -0600

    clarify src path for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './components/**/*.{html,js}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
