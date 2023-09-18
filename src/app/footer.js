77eef99b927e0cc1db55436a607a5d94e59b93a0
Adam Tak <adam.tak2112@gmail.com>
2023-09-17 21:44:20 -0600

    Fix for container inheritance bug

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,15 +1,25 @@
+
+"use client"
+
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from './footer.module.css';
 
 export default function Footer(){
     return (
         <footer>
-        <div className={styles.footerContainer}>
-            <div className='branding'>
-                <h5 className='brand-name'>Tak Software</h5>
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col>
+                    <div className={styles.footerContainer}>
+                        <h5 className='brand-name'>Tak Software</h5>
+                    </div>
+                    </Col>
+                </Row>
             
-        </div>
+            
+            </Container>
         </footer>
     );
 }
