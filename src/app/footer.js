dfe9174ba0cd8e93f6c3eec4fe055880ee090930
Adam Tak <adam.tak2112@gmail.com>
2023-09-27 12:27:37 -0600

    remove extra brand

--- src/app/footer.js
+++ src/app/footer.js
@@ -4,8 +4,8 @@
 export default function Footer(){
     return (
         <footer className="min-h-[8%] mx-[5rem]">
-            <div className="sticky bottom-0 columns-2 h-full bg-[#e5ca8c] w-full justify-center flex items-center space-x-[25px]">
-                    <h5 className='text-md'>Tak Software</h5>
+            <div className="sticky bottom-0 columns-2 h-full w-full justify-around flex items-center space-x-[25px]">
+                    
                     <h6 className="text-sm">© Tak Software 2023</h6>
             </div>
         </footer>
