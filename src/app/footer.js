21c41973f02d3c26668091d57ac0a8beb752418f
Adam Tak <adam.tak2112@gmail.com>
2023-09-27 12:29:38 -0600

    add variable date on bottom of footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -2,11 +2,13 @@
 "use client"
 
 export default function Footer(){
+    const today = new Date();
+
     return (
         <footer className="min-h-[8%] mx-[5rem]">
             <div className="sticky bottom-0 columns-2 h-full w-full justify-around flex items-center space-x-[25px]">
                     
-                    <h6 className="text-sm">© Tak Software 2023</h6>
+                    <h6 className="text-sm">© Adam Tak {today.getFullYear()}</h6>
             </div>
         </footer>
     );
