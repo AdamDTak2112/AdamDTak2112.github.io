860f3b40429b779fb6ddebefd37d75ee18ad83b0
Adam Tak <adam.tak2112@gmail.com>
2024-04-10 11:20:21 -0600

    project container initiated

---
+++ src/components/ProjectContainer.js
@@ -0,0 +1,18 @@
+
+const ProjectContainer = () => {
+    const projectUrls = [
+        'https://quote-machine-six.vercel.app/',
+        'https://roman-numeral.vercel.app/'
+    ];
+
+    const displayProjects = () => {
+
+    }
+    return (
+        <div className="projects">
+            <h1>Projects</h1>
+        </div>
+    );
+};
+
+export default ProjectContainer;
